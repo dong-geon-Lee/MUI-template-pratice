a5fd7af1fa9d89c08d6d5c0bead10d8944750d95
dong-geon-Lee <ch9716@naver.com>
2023-07-18 22:25:11 +0900

    Header AppBar 애니메이션 컴포넌트 구현 완료

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -2,19 +2,26 @@ import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
+// material-ui
 import { useTheme } from '@mui/material/styles';
 import { useMediaQuery, Box, Container, Toolbar } from '@mui/material';
 
+// project import
 import Drawer from './Drawer';
 import Header from './Header';
 import Footer from './Footer';
 import HorizontalBar from './Drawer/HorizontalBar';
+import Breadcrumbs from 'components/@extended/Breadcrumbs';
 
+import navigation from 'menu-items';
 import useConfig from 'hooks/useConfig';
+import { openDrawer } from 'store/reducers/menu';
 
+// types
 import { RootStateProps } from 'types/root';
 import { LAYOUT_CONST } from 'types/config';
-import { openDrawer } from 'store/reducers/menu';
+
+// ==============================|| MAIN LAYOUT ||============================== //
 
 const MainLayout = () => {
   const theme = useTheme();
@@ -29,22 +36,25 @@ const MainLayout = () => {
   const menu = useSelector((state: any) => state.menu);
   const { drawerOpen } = menu;
 
+  // drawer toggler
   const [open, setOpen] = useState(!miniDrawer || drawerOpen);
-
   const handleDrawerToggle = () => {
     setOpen(!open);
     dispatch(openDrawer({ drawerOpen: !open }));
   };
 
+  // set media wise responsive drawer
   useEffect(() => {
     if (!miniDrawer) {
       setOpen(!matchDownLG);
       dispatch(openDrawer({ drawerOpen: !matchDownLG }));
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [matchDownLG]);
 
   useEffect(() => {
     if (open !== drawerOpen) setOpen(drawerOpen);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [drawerOpen]);
 
   return (
@@ -65,6 +75,7 @@ const MainLayout = () => {
             flexDirection: 'column'
           }}
         >
+          <Breadcrumbs navigation={navigation} title titleBottom card={false} divider={false} />
           <Outlet />
           <Footer />
         </Container>
